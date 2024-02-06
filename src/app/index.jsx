f2d6736b46c26c07dd950763f6b096a7f9bb93f2
Oleksandr Leonov <oleksandr.leonov.work@gmail.com>
2024-02-06 21:37:20 +0200

    Initial configuration

---
+++ src/app/index.jsx
@@ -0,0 +1,10 @@
+import {Cryptocurrencies} from "../pages/cryptocurrencies/index.js";
+function App() {
+    return (
+        <>
+            <Cryptocurrencies/>
+        </>
+    )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        "./index.html",
+        "./src/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+        extend: {
+            'animation': {
+                'text': 'text 4s ease infinite alternate',
+            },
+            'keyframes': {
+                'text': {
+                    '0%, 100%': {
+                        'background-position': '0% 50%',
+                        'background-size': '300%'
+                    },
+                    '50%': {
+                        'background-position': '100% 50%',
+                        'background-size': '300%'
+                    }
+                },
+            }
+        },
+    },
+    plugins: [],
+}
+
